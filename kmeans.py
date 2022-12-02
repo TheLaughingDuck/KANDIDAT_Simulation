3b865fc0b7b5a5f800e084363a55635d7c146f6e
Simon Jorstedt <jorstedtsimon@gmail.com>
2022-12-02 15:26:34 +0100

    Wrote a wrapper function kmeans_func.
    Commiting for Backup.

--- kmeans.py
+++ kmeans.py
@@ -1,11 +1,17 @@
 from sklearn.cluster import KMeans
 import seeds
+import math
 
 
 '''Program file for running the k-means algorithm.'''
 
 
-def kmeans(data, seed_method, K, nstarts=1):
+def kmeans_func(data, seed_method, K, nstarts=1):
+    # list of loss values calculated for every start.
+    loss_list = []
+    
+    # list of "finished" centres after every start.
+    finished_centers = []
 
     for i in range(nstarts):
         #Run one iteration of the algorithm
@@ -21,8 +27,42 @@ def kmeans(data, seed_method, K, nstarts=1):
             raise Exception("Bad seed_method input. Use one of \{\"random\", \"plusplus\", \"kaufman\"\}")
     
         kmeans_object = KMeans(n_clusters = K, init=seed, n_init=1, max_iter=100).fit(data)
+        centers = kmeans_object.cluster_centers_
 
-        # We still have to compare the load/cost/weight of each run.
+        ### Calculate load/loss/cost/weight
+
+        # list of cluster-association index
+        index_list = []
+
+        # list of closest distance
+        shortest_D_list = []
+        
+        # Assign all points to their closest cluster (keep track using index).
+        for i in range(len(data)):
+            shortest_D = None
+            center_index = None
+            for j in range(len(seed)):
+                # Calculate D-istance
+                D = math.dist(data[i], seed[j])
+
+                if shortest_D == None:
+                    shortest_D = D
+                elif D < shortest_D:
+                    shortest_D = D
+                    center_index = j
+            # Keep track of closest index and distance
+            index_list.append(j)
+            shortest_D_list.append(shortest_D)
+        
+        # Calculate cost
+        COST = sum([i**2 for i in shortest_D_list])
+
+        loss_list.append(COST)
+        finished_centers.append(centers)
     
+    ### Select the set of centers with lowest associated COST
+    best_centers = finished_centers[loss_list.index(min(loss_list))]
+
+    ### Possibly make a plot?
     
-    return kmeans_object.cluster_centers_
+    return best_centers
--- main.py
+++ main.py
@@ -5,24 +5,23 @@ from sklearn.cluster import KMeans
 # Local packages
 import seeds
 import random_data
+import kmeans
 
 
 # Create or Load Data
-points = random_data.make_clusters(100, d=2, K=5)
+points = random_data.make_clusters(1000, d=2, K=50)
 
 # Run Initialization
-seed = seeds.plusplus(points, K=5)
+#seed = seeds.plusplus(points, K=10)
 
 
 # Run k-means
 #print("Printing seed")
 #print(seed)
 
-kmeans=KMeans(n_clusters = 5, init=seed, n_init=1).fit(points)
+#kmeans=KMeans(n_clusters = 5, init=seed, n_init=1).fit(points)
 #clusters=KMeans(n_clusters = 5, n_init=1).fit(points)
-
-
-centers = kmeans.cluster_centers_
+centers = kmeans.kmeans_func(points, seed_method="plusplus", K=50, nstarts=10)
 
 print(centers)
 
@@ -35,7 +34,7 @@ if len(points[0]) == 2:
     plt.scatter(points[:,0], points[:,1], marker=".")
     
     #Plot seed
-    plt.scatter(seed[:,0], seed[:,1], color="red", marker=".")
+    #plt.scatter(seed[:,0], seed[:,1], color="red", marker=".")
 
     #Plot final centers
     plt.scatter(centers[:,0], centers[:,1], color="orange", marker="*")
